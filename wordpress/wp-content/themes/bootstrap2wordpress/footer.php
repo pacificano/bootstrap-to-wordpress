82e9f741ff3978fb38839e9ea3aa0ae2363eb84c
pacificano <tris.pacificano@gmail.com>
2015-09-11 21:46:15 +1000

    wordpress theme setup

---
+++ wordpress/wp-content/themes/bootstrap2wordpress/footer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Bootstrap to Wordpress
+ */
+
+?>
+
+<?php wp_footer(); ?>
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="<?php bloginfo('template_directory'); ?>/assets/js/bootstrap.min.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/assets/js/main.js"></script>
+
+</body>
+</html>
