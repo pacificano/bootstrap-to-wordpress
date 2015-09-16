916c5e87946d24ce97f772afa11677477f3927f8
pacificano <tris.pacificano@gmail.com>
2015-09-16 11:13:55 +1000

    custom fields in the page

--- wordpress/wp-content/themes/bootstrap2wordpress/page-home.php
+++ wordpress/wp-content/themes/bootstrap2wordpress/page-home.php
@@ -3,6 +3,15 @@
 	Template Name: Home Page
 */
 
+// Custom Fields
+	$prelaunch_price = get_post_meta( 11, 'prelaunch_price', true );
+	$launch_price = get_post_meta( 11, 'launch_price', true );
+	$final_price = get_post_meta( 11, 'final_price', true );
+	$course_url = get_post_meta( 11, 'course_url', true );
+	$button_text = get_post_meta( 11, 'button_text', true );
+	$optin_text = get_post_meta( 11, 'optin_text', true );
+	$optin_button_text = get_post_meta( 11, 'optin_button_text', true );
+
 get_header(); ?>
 
 	<!-- hero -->
@@ -16,25 +25,25 @@ get_header(); ?>
     				</div><!-- col -->
 
     				<div class="col-sm-7 hero-text">
-    					<h1>I Love Cats SuperFriends</h1>
-    					<p class="lead">We are the best people in the world because we love cats. This is our website where other superfriends get together and discuss things.</p>
+    					<h1><?php bloginfo('name'); ?></h1>
+    					<p class="lead"><?php bloginfo('description'); ?></p>
 
     					<div id="price-timeline">
     						<div class="price active">
     							<h4>Pre-Launch<small>Price ends soon</small></h4>
-    							<span>$149</span>
+    							<span><?php echo $prelaunch_price; ?></span>
     						</div><!-- price -->
     						<div class="price">
     							<h4>Launch price<small>Coming soon!</small></h4>
-    							<span>$299</span>
+    							<span><?php echo $launch_price; ?></span>
     						</div><!-- price -->
     						<div class="price">
     							<h4>Final price<small>Coming soon!</small></h4>
-    							<span>$399</span>
+    							<span><?php echo $final_price; ?></span>
     						</div><!-- price -->
     					</div><!-- price-timeline -->
 
-    					<p><a href="/" class="btn btn-lg btn-danger" role="button">Enroll &raquo;</a></p>
+    					<p><a href="<?php echo $course_url; ?>" class="btn btn-lg btn-danger" role="button"><?php echo $button_text; ?></a></p>
 
     				</div><!-- col -->
 
@@ -53,11 +62,11 @@ get_header(); ?>
 			<div class="row">
 				
 				<div class="col-sm-8">
-					<p class="lead"><strong>Subscribe to our mailing list.</strong> We'll send something special as a thank you.</p>
+					<p class="lead"><?php echo $optin_text; ?></p>
 				</div><!-- col -->
 
 				<div class="col-sm-4">
-					<button class="btn btn-success btn-lg btn-block" data-toggle="modal" data-target="#myModal">Click here to subscribe</button>
+					<button class="btn btn-success btn-lg btn-block" data-toggle="modal" data-target="#myModal"><?php echo $optin_button_text; ?></button>
 				</div><!-- col -->
 
 			</div>
