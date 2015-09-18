b2f4b334d6c58fa1427a90ae1b333505fd593800
pacificano <tris.pacificano@gmail.com>
2015-09-18 14:11:18 +1000

    updated resources page

--- wordpress/wp-content/themes/bootstrap2wordpress/page-resources.php
+++ wordpress/wp-content/themes/bootstrap2wordpress/page-resources.php
@@ -4,6 +4,7 @@
 get_header();
 
 $thumbnail_url = wp_get_attachment_url(get_post_thumbnail_id($post->ID));
+
 ?>
 
 <!-- feature image -->
@@ -22,34 +23,35 @@ $thumbnail_url = wp_get_attachment_url(get_post_thumbnail_id($post->ID));
     <div class="row" id="primary">
         <div id="content" class="col-sm-12">
             <section class="main-content">
-                <p class="lead">Welcome to the Cat Superfriends page! Locavore retro viral, Shoreditch vegan cardigan gentrify chambray typewriter tilde single-origin coffee drinking vinegar iPhone lomo. Tattooed freegan YOLO, stumptown yr Pitchfork kitsch. Keffiyeh raw denim <strong>literally readymade Williamsburg kogi</strong>. Umami vegan mlkshk, fashion axe scenester Odd Future literally typewriter Brooklyn PBR sartorial kitsch leggings keytar asymmetrical.</p>
+                
+                <?php while (have_posts()) : the_post(); ?>
+					<?php the_content(); ?>
+                <?php endwhile; ?>
 
-                <p><em>Please keep in mind that some of the following resources are my affiliate links, and should you decide to make a purchase, I will earn a small commission &ndash; at no extra cost to you! I am recommeding these resources only because I have found them to be incredibly helpful. Don&rsquo;t spend your money on any products or resources unless you think they will benefit you.</em></p>
+                <?php $loop = new WP_Query( array( 'post_type' => 'resource', 'orderby' => 'post_id', 'order' => 'ASC') ); ?>
 
                 <hr>
 
                 <div class="resource-row clearfix">
 
-                    <div class="resource">
-                        <img src="assets/img/logo_petbarn.jpg">
-                        <h3><a href="http://www.petbarn.com.au">Petbarn</a></h3>
-                        <p>Fanny pack meditation Brooklyn, VHS pour-over craft beer next level street art four dollar toast. Squid Odd Future before they sold out shabby chic, tilde Banksy tattooed health goth Tumblr pop-up fap occupy. Umami Odd Future cardigan single-origin coffee, art party Banksy street art migas try-hard jean shorts Portland kale chips normcore bicycle rights lo-fi. Synth Bushwick distillery scenester twee hashtag actually plaid selfies pickled whatever freegan, authentic Godard raw denim. Austin pickled dreamcatcher, you probably haven't heard of them synth mustache biodiesel tofu kitsch cardigan migas whatever drinking vinegar fanny pack next level. Meggings 90's mumblecore, sriracha fingerstache chambray polaroid Wes Anderson Bushwick pour-over irony McSweeney's pug. Brooklyn forage leggings, fap farm-to-table street art kitsch pork belly cliche selvage American Apparel +1.</p>
-                        <a href="http://www.petbarn.com.au" class="btn btn-success">Buy cat food at Petbarn</a>
-                    </div><!-- resource -->
+                	<?php while($loop->have_posts()) : $loop->the_post(); ?>
+                	<?php 
+	                	$resource_image = get_field('resource_image');
+						$resource_url = get_field('resource_url');
+						$button_text = get_field('button_text'); 
+					?>
 
-                    <div class="resource">
-                        <img src="assets/img/logo_scienceDiet.png">
-                        <h3><a href="http://www.sciencediet.com.au">Science Diet</a></h3>
-                        <p>Fanny pack meditation Brooklyn, VHS pour-over craft beer next level street art four dollar toast. Squid Odd Future before they sold out shabby chic, tilde Banksy tattooed health goth Tumblr pop-up fap occupy. Umami Odd Future cardigan single-origin coffee, art party Banksy street art migas try-hard jean shorts Portland kale chips normcore bicycle rights lo-fi. Synth Bushwick distillery scenester twee hashtag actually plaid selfies pickled whatever freegan, authentic Godard raw denim. Austin pickled dreamcatcher, you probably haven't heard of them synth mustache biodiesel tofu kitsch cardigan migas whatever drinking vinegar fanny pack next level. Meggings 90's mumblecore, sriracha fingerstache chambray polaroid Wes Anderson Bushwick pour-over irony McSweeney's pug. Brooklyn forage leggings, fap farm-to-table street art kitsch pork belly cliche selvage American Apparel +1.</p>
-                        <a href="http://www.sciencediet.com.au" class="btn btn-success">The best cat food is Science Diet</a>
-                    </div><!-- resource -->
+	                    <div class="resource">
+	                    	<img src="<?php echo $resource_image[url]; ?>" alt="<?php echo $resource_image[alt]; ?>">
+	                        <h3><a href="<?php echo $resource_url; ?>"><?php the_title(); ?></a></h3>
+	                        <?php the_content(); ?>
 
-                    <div class="resource">
-                        <img src="assets/img/logo_kwitter.jpg">
-                        <h3><a href="http://www.kittykwitter.com.au">Kitty Kwitter</a></h3>
-                        <p>Fanny pack meditation Brooklyn, VHS pour-over craft beer next level street art four dollar toast. Squid Odd Future before they sold out shabby chic, tilde Banksy tattooed health goth Tumblr pop-up fap occupy. Umami Odd Future cardigan single-origin coffee, art party Banksy street art migas try-hard jean shorts Portland kale chips normcore bicycle rights lo-fi. Synth Bushwick distillery scenester twee hashtag actually plaid selfies pickled whatever freegan, authentic Godard raw denim. Austin pickled dreamcatcher, you probably haven't heard of them synth mustache biodiesel tofu kitsch cardigan migas whatever drinking vinegar fanny pack next level. Meggings 90's mumblecore, sriracha fingerstache chambray polaroid Wes Anderson Bushwick pour-over irony McSweeney's pug. Brooklyn forage leggings, fap farm-to-table street art kitsch pork belly cliche selvage American Apparel +1.</p>
-                        <a href="http://www.kittykwitter.com.au" class="btn btn-success">Toilet train your cat</a>
-                    </div><!-- resource -->
+	                        <?php if(!empty($button_text)) : ?>
+		                        <a href="<?php echo $resource_url; ?>" class="btn btn-success"><?php echo $button_text; ?></a>
+		                    <?php endif; ?>
+	                    </div><!-- resource -->
+
+                    <?php endwhile; ?>
 
                 </div><!-- resource-row clearfix -->
             </section><!-- main-content -->
